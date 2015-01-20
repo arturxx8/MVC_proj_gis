f8d408f63e707a1b4944564842f2896d73587648
slav-yrich <slav-yrich@rambler.ru>
2015-01-21 01:35:05 +0300

    Делаем категорию без ссылки если пуста

--- app/models/Catalog.php
+++ app/models/Catalog.php
@@ -25,6 +25,29 @@ class Catalog extends Eloquent  {
         }
         return '';
     }
+    static public function tree($cats,$parent_id,$only_parent = false){
+    if(is_array($cats) and isset($cats[$parent_id])){
+        $tree = '<ul class="sub-menu">';
+        if($only_parent==false){
+            foreach($cats[$parent_id] as $cat){
+                if (Book::where('category','=',$cat['id'])->count()==0)
+                    $tree .= '<li >'.'<p class="title">'.$cat['category_name'].'</p>';
+                else
+                    $tree .= '<li >'.'<p class="title">'.'<a class="title" href="'.URL::to("catalog").'/'.$cat['id'].'">'.$cat['category_name'].'</a>'.'</p>';
+                $tree .=  Catalog::tree($cats,$cat['id']);
+                $tree .= '</li>';
+            }
+        }elseif(is_numeric($only_parent)){
+            $cat = $cats[$parent_id][$only_parent];
+            $tree .= '<li>'.'<p class="title">'.'<a class="title" href="'.URL::to("catalog").'/'.$cat['id'].'">'.$cat['category_name'].'</a>'.'</p>';
+            $tree .=  Catalog::tree($cats,$cat['id']);
+            $tree .= '</li>';
+        }
+        $tree .= '</ul>';
+    }
+    else return null;
+    return $tree;
+    }
 
     static function allCategoryList()
     {
--- app/views/catalog.blade.php
+++ app/views/catalog.blade.php
@@ -12,30 +12,7 @@
         <h1>Каталог</h1>
         <ul class="thumbnails newBooks">
             <div class="clearfix">
-                <?
-                function tree($cats,$parent_id,$only_parent = false){
-                    if(is_array($cats) and isset($cats[$parent_id])){
-                        $tree = '<ul class="sub-menu">';
-                        if($only_parent==false){
-                            foreach($cats[$parent_id] as $cat){
-                                $tree .= '<li >'.'<p class="title">'.'<a class="title" href="'.URL::to("catalog").'/'.$cat['id'].'">'.$cat['category_name'].'</a>'.'</p>';
-
-                                $tree .=  tree($cats,$cat['id']);
-                                $tree .= '</li>';
-                            }
-                        }elseif(is_numeric($only_parent)){
-                            $cat = $cats[$parent_id][$only_parent];
-                            $tree .= '<li>'.'<p class="title">'.$cat['category_name'].'</p>';
-                            $tree .=  tree($cats,$cat['id']);
-                            $tree .= '</li>';
-                        }
-                        $tree .= '</ul>';
-                    }
-                    else return null;
-                    return $tree;
-                }
-                ?>
-                {{tree(CatalogController::Catalogs(),0)}}
+                {{Catalog::tree(CatalogController::Catalogs(),0)}}
             </div>
         </ul>
 
